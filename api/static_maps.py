08b8749e8d19ba96d41681b15bbaf5b2215c5751
Станислав Некрасов <nek.stas.other@yandex.ru>
2022-12-16 11:48:57 +0300

    Добавлен метод для отображения топонима на карте

---
+++ api/static_maps.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from api.geocoder import get_toponym_rect
+
+STATIC_MAPS_URL = 'https://static-maps.yandex.ru/1.x/'
+
+
+def get_toponym_map_url(toponym):
+    rect = get_toponym_rect(toponym)
+
+    return STATIC_MAPS_URL + \
+        f'?l=map' \
+        f'&lang=ru_RU' \
+        f'&bbox={rect[0]},{rect[1]}~{rect[2]},{rect[3]}'
