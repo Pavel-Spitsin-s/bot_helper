f948d4fa22c2a31c2a58edd2d7416454257738b8
Alex-50 <74928928+Alex-50@users.noreply.github.com>
2022-12-18 12:23:39 +0300

    Fixed some bugs

--- parse/parse_date.py
+++ parse/parse_date.py
@@ -2,8 +2,11 @@ from parse.delta_date import delta_date
 from parse.fix_date import fix_date
 
 
-def get_date(text):
-    if 'через' in text:
-        return delta_date(text)
-    else:
-        return fix_date(text)
+def get_date(text, cur_date):
+	text = text.split()
+	text[0] = text[0].lower()
+	text = ' '.join(text)
+	if 'через' in text:
+		return delta_date(text, cur_date)
+	else:
+		return fix_date(text, cur_date)
