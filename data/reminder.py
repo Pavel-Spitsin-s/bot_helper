ecf671f48d764dec7f188a3856bb70565dbd37b0
Alex-50 <74928928+Alex-50@users.noreply.github.com>
2022-12-19 11:29:46 +0300

    Tabs

--- data/reminder.py
+++ data/reminder.py
@@ -5,10 +5,10 @@ from .db_session import SqlAlchemyBase
 
 
 class Reminder(SqlAlchemyBase, SerializerMixin):
-	__tablename__ = 'reminders'
-	id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, unique=True)
-	user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
-	target_time = sqlalchemy.Column(sqlalchemy.DateTime)
-	action = sqlalchemy.Column(sqlalchemy.Text)
+    __tablename__ = 'reminders'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, unique=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    target_time = sqlalchemy.Column(sqlalchemy.DateTime)
+    action = sqlalchemy.Column(sqlalchemy.Text)
 
-	user = orm.relation('User')
+    user = orm.relation('User')
--- data/user.py
+++ data/user.py
@@ -4,9 +4,9 @@ from .db_session import SqlAlchemyBase
 
 
 class User(SqlAlchemyBase, SerializerMixin):
-	__tablename__ = 'users'
-	id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, unique=True)
-	tg_id = sqlalchemy.Column(sqlalchemy.Integer, unique=True)
-	lat = sqlalchemy.Column(sqlalchemy.String)
-	long = sqlalchemy.Column(sqlalchemy.String)
-	name = sqlalchemy.Column(sqlalchemy.String)
+    __tablename__ = 'users'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, unique=True)
+    tg_id = sqlalchemy.Column(sqlalchemy.Integer, unique=True)
+    lat = sqlalchemy.Column(sqlalchemy.String)
+    long = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String)
