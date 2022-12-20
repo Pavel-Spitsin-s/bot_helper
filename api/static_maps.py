605d1e41b7d1a1d0835d6c424f4c5e589d2ec643
nekstas <nek.stas.other@yandex.ru>
2022-12-20 10:02:01 +0300

    Исправлены опечатки

--- api/static_maps.py
+++ api/static_maps.py
@@ -9,9 +9,9 @@ STATIC_MAPS_URL = 'https://static-maps.yandex.ru/1.x/'
 def get_toponym_map_url(toponym):
     rect = get_toponym_rect(toponym)
 
-    return STATIC_MAPS_URL + urllib.parse.urlencode({
+    return STATIC_MAPS_URL + '?' + urllib.parse.urlencode({
         'l': 'map',
         'lang': 'ru_RU',
-        'size': '400x300',
+        'size': '400,300',
         'bbox': f'{rect[0]},{rect[1]}~{rect[2]},{rect[3]}'
     })
