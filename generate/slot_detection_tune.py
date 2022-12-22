13e988d6293f2a8d1ddeda3bce5ae7f674674064
Pavel-Spitsin-s <66683597+Pavel-Spitsin-s@users.noreply.github.com>
2022-12-22 10:06:49 +0300

    Update slot_detection_tune.py

--- generate/slot_detection_tune.py
+++ generate/slot_detection_tune.py
@@ -4,10 +4,19 @@ ttm.set_api_key(
     'API_KEY'
 )
 
-base_slot = ttm.TuneTheModel.from_id("3wp7he0qx1577o8jsowqb00k1gcrmo3l")
+IDS = {
+    "base_model": "3wp7he0qx1577o8jsowqb00k1gcrmo3l",  # стандартная обученная модель
+    "ru_model": "3wp7he0qx1577o8jsowqb00k1gcrmo3l",  # модель доученная на датасете из 16k сообщений
+}
+
+base_slot = ttm.TuneTheModel.from_id(IDS["ru_model"])
 
 
 def message_to_tag(text: str) -> dict:
+    """
+    :param text: message to tagging
+    :return: dictionary tag -> tag content
+    """
     response = base_slot.generate(text)
     d = dict()
     open_brackets = []
