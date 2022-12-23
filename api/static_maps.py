1e123db154fbc31fcbd60aa3b227e69c43c53d55
nekstas <nek.stas.other@yandex.ru>
2022-12-23 12:47:31 +0300

    URL для отображения карты теперь генерируется по-другому

--- api/static_maps.py
+++ api/static_maps.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 import urllib.parse
 
-from api.geocoder import get_toponym_rect
+from api.geocoder import get_toponym_rect, get_toponym_coords, get_toponym_spn
 
 STATIC_MAPS_URL = 'https://static-maps.yandex.ru/1.x/'
 
 
 def get_toponym_map_url(toponym):
-    rect = get_toponym_rect(toponym)
+    spn = get_toponym_spn(toponym)
+    coords = get_toponym_coords(toponym)
 
     return STATIC_MAPS_URL + '?' + urllib.parse.urlencode({
         'l': 'map',
         'lang': 'ru_RU',
         'size': '400,300',
-        'bbox': f'{rect[0]},{rect[1]}~{rect[2]},{rect[3]}'
+        'll': f'{coords[0]},{coords[1]}',
+        'spn': f'{spn[0]},{spn[1]}'
     })
