72fef785bf87205d6ca61cd4e36b8e5795c6bafe
Kirill <ept-mat@mail.ru>
2022-12-23 23:06:32 +0300

    add try except in classifier

--- classifier.py
+++ classifier.py
@@ -11,20 +11,22 @@ from generate.slot_detection_tune import message_to_tag
 
 async def classify(text, user):
     tags = {}
-
-    if ('anek' in tags.keys()) or ('анек' in text):
-        return ['анекдот', await get_joke()]
-    elif ('film' in tags.keys()) or ('фильм' in text):
-        return ['фильм', await select_film()]
-    elif ('serial' in tags.keys()) or ('сериал' in text):
-        return ['сериал', await select_series()]
-    elif (any(w in tags.keys() for w in ('news', 'last_news'))) or ('новост' in text):
-        return ['новость', await last_news()]
-    elif any(w in tags.keys() for w in ['currency', 'currency_name']) or any(w in text for w in ['курс', 'доллар сша']):
-        return ['валюты', await currencies_handler(text)]
-    elif ('reminder' in tags.keys()) or any(w in text for w in ['напомн', 'напомин']):
-        return ['напоминание', await add_reminder(text, user)]
-    elif 'weather_descriptor' in tags.keys() or 'погода' in text:
-        return ['погода', await weather_handler(text, user)]
-    else:
-        return ['болталка', await get_next_sequence(text, user, 0)]
+    try:
+        if ('anek' in tags.keys()) or ('анек' in text):
+            return ['анекдот', await get_joke()]
+        elif ('film' in tags.keys()) or ('фильм' in text):
+            return ['фильм', await select_film()]
+        elif ('serial' in tags.keys()) or ('сериал' in text):
+            return ['сериал', await select_series()]
+        elif (any(w in tags.keys() for w in ('news', 'last_news'))) or ('новост' in text):
+            return ['новость', await last_news()]
+        elif any(w in tags.keys() for w in ['currency', 'currency_name']) or any(w in text for w in ['курс', 'доллар сша']):
+            return ['валюты', await currencies_handler(text)]
+        elif ('reminder' in tags.keys()) or any(w in text for w in ['напомн', 'напомин']):
+            return ['напоминание', await add_reminder(text, user)]
+        elif 'weather_descriptor' in tags.keys() or 'погода' in text:
+            return ['погода', await weather_handler(text, user)]
+        else:
+            return ['болталка', await get_next_sequence(text, user, 0)]
+    except Exception:
+        return ['ошибка', 'Извините, на сервере какие-то неполадки, попробуйте спросить что-нибудь другое.']
